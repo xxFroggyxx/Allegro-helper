ea9788380057e65f724511ee80d730620ca9f96e
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-12-29 20:50:30 +0100

    feat: created setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,20 @@
+from os import path
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    REQUIRES = f.readlines()
+
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="Allegro Sale Helper",
+    version="0.0.1",
+    author="Wojciech Klimczewski",
+    description="Helps with sales management on Allegro",
+    long_description=read("README.md"),
+    packages=find_packages(),
+    install_requires=REQUIRES,
+)
