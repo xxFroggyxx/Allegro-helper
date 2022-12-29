e1ed4798c5046eb3cbddb003bed2bd5a146f41c5
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-12-29 20:52:09 +0100

    feat: create basic code

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests
+import json
+import time
+import os
+from dotenv import load_dotenv
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+load_dotenv()
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+CODE_URL = "https://allegro.pl/auth/oauth/device"
+TOKEN_URL = "https://allegro.pl/auth/oauth/token"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def get_code():
+    try:
+        payload = {'client_id': CLIENT_ID}
+        headers = {'Content-type': 'application/x-www-form-urlencoded'}
+        api_call_response = requests.post(CODE_URL, auth=(CLIENT_ID, CLIENT_SECRET),
+                                          headers=headers, data=payload, verify=True)
+        return api_call_response
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+
+def get_access_token(device_code):
+    try:
+        headers = {'Content-type': 'application/x-www-form-urlencoded'}
+        data = {'grant_type': 'urn:ietf:params:oauth:grant-type:device_code', 'device_code': device_code}
+        api_call_response = requests.post(TOKEN_URL, auth=(CLIENT_ID, CLIENT_SECRET),
+                                          headers=headers, data=data, verify=True)
+        return api_call_response
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+
+def await_for_access_token(interval, device_code):
+    while True:
+        time.sleep(interval)
+        result_access_token = get_access_token(device_code)
+        token = json.loads(result_access_token.text)
+        if result_access_token.status_code == 400:
+            if token['error'] == 'slow_down':
+                interval += interval
+            if token['error'] == 'access_denied':
+                break
+        else:
+            return token['access_token']
+
+
+def get_orders(token):
+    try:
+        url = "https://api.allegro.pl/order/checkout-forms"
+        headers = {'Authorization': 'Bearer ' + token, 'Accept': "application/vnd.allegro.public.v1+json"}
+        orders_result = requests.get(url, headers=headers, verify=True)
+        return orders_result
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+
+def main():
+    code = get_code()
+    result = json.loads(code.text)
+    print(result)
+    print("User, open this address in the browser:" + result['verification_uri_complete'])
+    access_token = await_for_access_token(int(result['interval']), result['device_code'])
+    # print("access_token = " + access_token)
+    orders = get_orders(access_token)
+    print(json.dumps(orders.json(), indent=4))
+
+
+if __name__ == "__main__":
+    main()
