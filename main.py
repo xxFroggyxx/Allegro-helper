a8a3ff44c4089bea2a2c0afd3c93551154fabaed
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2023-01-25 19:50:21 +0100

    feat: create main function and add basic types + docstrings

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import os
 import datetime
 from dotenv import load_dotenv
+from collections import OrderedDict
 
 load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
@@ -20,6 +21,7 @@ if os.path.exists("expiring.txt"):
 
 
 def get_code():
+    """Returns necessary device_code"""
     try:
         payload = {'client_id': CLIENT_ID}
         headers = {'Content-type': 'application/x-www-form-urlencoded'}
@@ -30,7 +32,13 @@ def get_code():
         raise SystemExit(err)
 
 
-def get_access_token(device_code):
+def get_access_token(device_code: str):
+    """
+    Helping method for await_for_access_token
+
+      device_code: str
+        necessary code for proper work.
+    """
     try:
         headers = {'Content-type': 'application/x-www-form-urlencoded'}
         data = {'grant_type': 'urn:ietf:params:oauth:grant-type:device_code', 'device_code': device_code}
@@ -41,7 +49,15 @@ def get_access_token(device_code):
         raise SystemExit(err)
 
 
-def await_for_access_token(interval, device_code):
+def await_for_access_token(interval: int, device_code: str):
+    """
+    Returns or saves essential access_token to communicate with API and do responses with user recognition.
+
+      interval: int
+        do interval as it names.
+      device_code: str
+        necessary code for proper work.
+    """
     while True:
         time.sleep(interval)
         result_access_token = get_access_token(device_code)
@@ -53,43 +69,119 @@ def await_for_access_token(interval, device_code):
                 break
         else:
             now = datetime.datetime.now()
-            expire_time = now + datetime.timedelta(hours=10)
+            expire_time = now + datetime.timedelta(seconds=43199)
             with open("expiring.txt", "w") as f:
                 f.write(f"EXPIRE_TIME>>>{expire_time.strftime('%Y-%m-%d %H:%M:%S')}\nACCESS_TOKEN>>>{token['access_token']}")
 
             return token['access_token']
 
 
-def get_orders(token):
+def get_all_orders(token: str):
+    """
+    Returns last 100 orders.
+
+      token: str
+        access token which enables to do queries.
+    """
     try:
         url = "https://api.allegro.pl/order/checkout-forms"
         headers = {'Authorization': 'Bearer ' + token, 'Accept': "application/vnd.allegro.public.v1+json"}
-        orders_result = requests.get(url, headers=headers, verify=True)
-        return orders_result
+        all_orders_result = requests.get(url, headers=headers, verify=True)
+        return all_orders_result.json()
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
 
 
+def get_orders(token: str):
+    """
+    Returns the last 100 orders which payment is done, and are ready to ship/shipped.
+
+      token: str
+        access token which enables to do queries.
+    """
+    try:
+        url = "https://api.allegro.pl/order/checkout-forms?status=READY_FOR_PROCESSING&fulfillment.status=SENT&fulfillment.status=READY_FOR_SHIPMENT"
+        headers = {'Authorization': 'Bearer ' + token, 'Accept': "application/vnd.allegro.public.v1+json"}
+        orders_result = requests.get(url, headers=headers, verify=True)
+        return orders_result.json()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+
+def get_orders_with_date_border(token: str, date: str, sort: str = "Descending"):
+    """
+    Returns sorted ordered items, with the required date option to enter the day from which day to count until today.
+
+      date: str
+        you enter the date that will be the end condition of this method.
+      sort: str ***OPTIONAL***
+        by default Descending. Choose from three sorting methods.
+          Alphabetical: Alphabetical, alphabetical, alph
+          Ascending: Ascending, ascending, asc
+          Descending: Descending, descending, desc
+    """
+    givenDate = datetime.datetime.strptime(date, "%Y-%m-%d")
+    print(f"Wszystkie przedmioty sprzedane do {givenDate.date()} włącznie:\n")
+    n = 0
+    count_products = {}
+    while True:
+        try:
+            url = f"https://api.allegro.pl/order/checkout-forms?status=READY_FOR_PROCESSING&fulfillment.status=SENT&fulfillment.status=READY_FOR_SHIPMENT&offset={n}"
+            headers = {'Authorization': 'Bearer ' + token, 'Accept': "application/vnd.allegro.public.v1+json"}
+            orders_date_result = requests.get(url, headers=headers, verify=True).json()
+
+            for ind in range(len(orders_date_result["checkoutForms"])):
+                productBoughtDate = datetime.datetime.strptime(orders_date_result["checkoutForms"][ind]["lineItems"][0]["boughtAt"][0:10], "%Y-%m-%d")
+
+                if givenDate > productBoughtDate:
+                    if sort in ["Alphabetical", "alphabetical", "alph"]:
+                        return OrderedDict(sorted(count_products.items(), key=lambda item: item[0]))
+                    elif sort in ["Ascending", "ascending", "asc"]:
+                        return OrderedDict(sorted(count_products.items(), key=lambda item: item[1]))
+                    elif sort in ["Descending", "descending", "desc"]:
+                        return OrderedDict(sorted(count_products.items(), key=lambda item: item[1], reverse=True))
+                    else:
+                        print("Popraw opcje sortu! Dostępne: alphabetical, ascending, descending")
+                        return
+
+                for x in range(len(orders_date_result["checkoutForms"][ind]["lineItems"])):
+                    if not orders_date_result["checkoutForms"][ind]["lineItems"][x]["offer"]["name"] in count_products:
+                        count_products[orders_date_result["checkoutForms"][ind]["lineItems"][x]["offer"]["name"]] = \
+                            orders_date_result["checkoutForms"][ind]["lineItems"][x]["quantity"]
+                    else:
+                        count_products[orders_date_result["checkoutForms"][ind]["lineItems"][x]["offer"]["name"]] += \
+                            orders_date_result["checkoutForms"][ind]["lineItems"][x]["quantity"]
+
+            n += 100
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
+
+
 def main():
     code = get_code()
-    result = json.loads(code.text)
+    dev = json.loads(code.text)
 
     if len(DATA) != 0:
         now = datetime.datetime.now()
         expire_time = datetime.datetime.strptime(DATA['EXPIRE_TIME'], '%Y-%m-%d %H:%M:%S')
 
         if now > expire_time:
-            print("User, open this address in the browser:" + result['verification_uri_complete'])
-            access_token = await_for_access_token(int(result['interval']), result['device_code'])
+            print("User, open this address in the browser:" + dev['verification_uri_complete'])
+            access_token = await_for_access_token(int(dev['interval']), dev['device_code'])
         else:
             access_token = DATA['ACCESS_TOKEN']
 
     else:
-        print("User, open this address in the browser:" + result['verification_uri_complete'])
-        access_token = await_for_access_token(int(result['interval']), result['device_code'])
+        print("User, open this address in the browser:" + dev['verification_uri_complete'])
+        access_token = await_for_access_token(int(dev['interval']), dev['device_code'])
 
-    orders = get_orders(access_token)
-    print(json.dumps(orders.json(), indent=4))
+    result = get_orders_with_date_border(access_token, "2023-01-25")
+    suma = 0
+    for key, value in result.items():
+        print(f"{key} x{value}")
+        suma += value
+    print("Suma sprzedanych przedmiotow", suma)
+    print("Ilosc unikalnych przedmiotow", len(result.items()))
 
 
 if __name__ == "__main__":
