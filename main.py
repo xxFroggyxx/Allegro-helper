051814658088461b50580e0750cbca9899fdcc06
xxFroggyxx <63168725+xxFroggyxx@users.noreply.github.com>
2023-01-28 04:13:58 +0100

    feat: create gui for app and main functionality

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import codecs
 import datetime
 import json
 import os
+import threading
 import time
+import tkinter
+import webbrowser
 from collections import OrderedDict
 
+import customtkinter
 import requests
 from dotenv import load_dotenv
 
@@ -158,32 +163,86 @@ def get_orders_with_date_border(token: str, date: str, sort: str = "Descending")
             raise SystemExit(err)
 
 
-def main():
-    if len(DATA) != 0:
-        now = datetime.datetime.now()
-        expire_time = datetime.datetime.strptime(DATA['EXPIRE_TIME'], '%Y-%m-%d %H:%M:%S')
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.access_token = None
+        # Settings
+        customtkinter.set_default_color_theme("green")
+        self.title("Allegro helper")
+        self.minsize(450, 450)
+        self.geometry("500x500")
 
-        if now > expire_time:
-            code = get_code()
-            dev = json.loads(code.text)
-            print("User, open this address in the browser:" + dev['verification_uri_complete'])
-            access_token = await_for_access_token(int(dev['interval']), dev['device_code'])
+        # Authorization layout
+        self.authorization_layout = customtkinter.CTkFrame(master=self, width=375, height=375, corner_radius=10)
+        self.label1 = customtkinter.CTkLabel(master=self.authorization_layout, text="Autoryzuj użytkownika", font=("Calibri", 32))
+        self.label1.place(relx=.5, rely=.3, anchor=tkinter.N)
+        self.authorizationButton = customtkinter.CTkButton(master=self.authorization_layout, text="Zaloguj", width=150, height=40,
+                                                           command=threading.Thread(target=self.authorize_user).start)
+        self.authorizationButton.place(relx=.5, rely=.6, anchor=tkinter.N)
+
+        # Functional layout
+        self.functional_layout = customtkinter.CTkFrame(master=self, width=375, height=375, corner_radius=10)
+        self.label2 = customtkinter.CTkLabel(master=self.functional_layout, text="Wybierz co chcesz zrobić", font=("Calibri", 32))
+        self.label2.place(relx=.5, rely=.3, anchor=tkinter.N)
+        self.optionButton = customtkinter.CTkButton(master=self.functional_layout, text="1. Pobierz liste produktów", height=32,
+                                                    command=threading.Thread(target=self.download_orders_with_date_border).start)
+        self.optionButton.place(relx=.5, rely=.6, anchor=tkinter.N)
+
+        if len(DATA) != 0:
+            self.current_layout = self.functional_layout
+            self.functional_layout.pack(expand=True)
+
+            now = datetime.datetime.now()
+            expire_time = datetime.datetime.strptime(DATA['EXPIRE_TIME'], '%Y-%m-%d %H:%M:%S')
+            if now > expire_time:
+                self.switch_layout()
+            else:
+                self.access_token = DATA['ACCESS_TOKEN']
+
         else:
-            access_token = DATA['ACCESS_TOKEN']
+            self.current_layout = self.authorization_layout
+            self.authorization_layout.pack(expand=True)
 
-    else:
+    def switch_layout(self):
+        if self.current_layout == self.authorization_layout:
+            self.authorization_layout.pack_forget()
+            self.functional_layout.pack(expand=True)
+            self.current_layout = self.functional_layout
+        else:
+            self.functional_layout.pack_forget()
+            self.authorization_layout.pack(expand=True)
+            self.current_layout = self.authorization_layout
+
+    def authorize_user(self):
         code = get_code()
         dev = json.loads(code.text)
-        print("User, open this address in the browser:" + dev['verification_uri_complete'])
-        access_token = await_for_access_token(int(dev['interval']), dev['device_code'])
+        webbrowser.open(dev['verification_uri_complete'])
+        self.access_token = await_for_access_token(int(dev['interval']), dev['device_code'])
+        self.switch_layout()
 
-    result = get_orders_with_date_border(access_token, "2023-01-25")
-    suma = 0
-    for key, value in result.items():
-        print(f"\t{key} x{value}")
-        suma += value
-    print("\nSuma sprzedanych przedmiotow", suma)
-    print("Ilosc unikalnych przedmiotow", len(result.items()))
+    def download_orders_with_date_border(self):
+        now = datetime.datetime.now().date()
+        date_dialog = customtkinter.CTkInputDialog(text="YYYY-MM-DD", title="Podaj date")
+        value_date_dialog = date_dialog.get_input()
+        if value_date_dialog == "" or len(value_date_dialog) < 9:
+            return
+
+        result = get_orders_with_date_border(self.access_token, str(value_date_dialog))
+        suma = 0
+        with codecs.open(f"{value_date_dialog} {now}.txt", "w", 'utf-8') as f:
+            f.write(f"Wszystkie przedmioty sprzedane do {value_date_dialog} włącznie:\n")
+            for key, value in result.items():
+                f.write(f"{key} x{value}\n")
+                suma += value
+            f.write(f"\nSuma sprzedanych przedmiotow {suma}\n")
+            f.write(f"Ilosc unikalnych przedmiotow {len(result.items())}")
+        print("Zapisano")
+
+
+def main():
+    app = App()
+    app.mainloop()
 
 
 if __name__ == "__main__":
